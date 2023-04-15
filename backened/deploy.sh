2990b93057b897cb23613d1b53c29b244c6cfa2b
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-04-15 17:05:40 +0200

    fix deploy.sh

--- backened/deploy.sh
+++ backened/deploy.sh
@@ -14,12 +14,12 @@ if [[ -z "${SSH_PORT}" ]]; then
     exit 1
 fi
 # Set the source and destination for the scp command
-DESTINATION_FOLDER="${SSH_SERVER}:/home/${SSH_USER}/BHL2023/backened/${SOURCE_FILE}"
+DESTINATION_FOLDER="${SSH_SERVER}:${SSH_PATH}/${SOURCE_FILE}"
 echo $DESTINATION_FOLDER
 
 # Transfer the file using scp
 scp -P "${SSH_PORT}" "${SOURCE_FILE}" "${DESTINATION_FOLDER}"
 
 ssh ${SSH_SERVER} "cd ~/BHL2023 && git pull"
-ssh ${SSH_SERVER} "cd ~/BHL2023/backened && ll; docker compose up --build -d"
+ssh ${SSH_SERVER} "cd ~/BHL2023/backened; docker compose up --build -d"
 
