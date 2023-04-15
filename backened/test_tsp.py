076b87d7b6d0ec830ed50c35434bafd8ac0c3076
mszawerd <01169246@pw.edu.pl>
2023-04-16 01:16:18 +0200

    added matrix

--- backened/test_tsp.py
+++ backened/test_tsp.py
@@ -1,11 +1,23 @@
 import numpy as np
 from python_tsp.exact import solve_tsp_dynamic_programming
 
-distance_matrix = np.array([
-    [0,  5, 4, 10],
-    [5,  0, 8,  5],
-    [4,  8, 0,  3],
-    [10, 5, 3,  0]
-])
+graph = {
+    0: [(2, 2), (3, 2), (6, 5), (7, 5)],
+    1: [(2, 1)],
+    2: [(3, 1), (6, 3), (7, 4)],
+    3: [(6, 4), (7, 3)],
+    4: [(3, 1)],
+    5: [(6, 1)],
+    6: [(7, 1)],
+    7: [(8, 1)],
+    8: []
+}
+
+
+distance_matrix = np.zeros((9, 9))
+for key, item in graph.items():
+    for n, d in item:
+        distance_matrix[key][d]
+
 permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
 print(permutation, distance)
