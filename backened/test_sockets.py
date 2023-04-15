e2d5e53fc5df794889083e594af5bec733118aeb
mszawerd <01169246@pw.edu.pl>
2023-04-15 17:09:54 +0200

    Added order endpoint

--- backened/test_sockets.py
+++ backened/test_sockets.py
@@ -2,8 +2,10 @@ import asyncio
 import websockets
 
 async def test():
-    async with websockets.connect("ws://localhost:8080") as websocket:
-        response = await websocket.recv()
-        print(response)
+    async with websockets.connect("ws://localhost:8080/work/hello") as websocket:
+        while True:
+            response = await websocket.recv()
+            print(response)
 
 asyncio.get_event_loop().run_until_complete(test())
+asyncio.get_event_loop().run_forever()
