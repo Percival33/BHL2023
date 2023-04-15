50b193a419db484736da20416ceab5857847dac9
mszawerd <01169246@pw.edu.pl>
2023-04-15 14:38:16 +0200

    added basic models

--- backened/models.py
+++ backened/models.py
@@ -1,6 +1,32 @@
+from datetime import datetime
+from typing import List
+
 from pydantic import BaseModel
 
 
-class UserModel(BaseModel):
+class Item:
+    code: str
     name: str
-    lastName: str
+    state: str
+    regal: int
+    column: int
+    shelf: int
+
+
+class Order:
+    items: List[Item]
+    date_order: datetime
+
+
+class Record:
+    items: List[Item]
+    date_assigned: datetime
+    date_collected: datetime
+    distance: int
+
+
+class Worker:
+    name: str
+    last_name: str
+    is_free: bool
+    orders: List[Order]
