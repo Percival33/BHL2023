c9f38f1d904858ed6e4a51b7cb7ab0a9f098ccb4
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-04-15 23:47:57 +0200

    handle defects

--- backened/connectors/UserConnector.py
+++ backened/connectors/UserConnector.py
@@ -11,3 +11,6 @@ class UserConnector(DatabaseConnector):
 
     def change_user_state(self, user, state):
         self.users_table.update_one({"_id": user['_id']}, {"$set": {"state": state}})
+
+    def get_user_by_username(self, user_id):
+        return self.users_table.find_one({"user_id": user_id})
--- backened/routers/user_router.py
+++ backened/routers/user_router.py
@@ -1,21 +1,44 @@
+import json
+
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
-from beans import user_manager
+from beans import user_manager, defect_connector, item_connector
+from models import RecordItemResponseType, Defect
+from datetime import datetime
 
 router = APIRouter()
 
 
-@router.websocket("/{username}")
-async def socket_test(websocket: WebSocket, username: str):
-    await user_manager.connect(websocket, username)
+@router.websocket("/{user_id}")
+async def socket_test(websocket: WebSocket, user_id: str):
+    await user_manager.connect(websocket, user_id)
     try:
         while True:
             response = await websocket.receive_json()
-            # if response["type"] == RecordItemType.finished_task:
-            #     finish_record(response["content"])
-            # elif response["type"] == RecordItemType.defect:
-            #     handle_defect(response["content"])
-            # elif response["type"] == RecordItemType.taken_product:
-            #     taken_product(response["content"])
+            msg = handle_response(response, user_id)
+            await websocket.send_json(json.dumps(str(msg)))
     except WebSocketDisconnect:
         print("Connection closed")
+
+
+def handle_response(response, user_id):
+    if response["type"] == RecordItemResponseType.DEFECT:
+        defect = Defect(
+            item_id=response["item_id"],
+            comment=response["content"],
+            date=str(datetime.timestamp(datetime.now())),
+            worker_id=user_id
+        )
+        # defect_connector.report_defect(defect)
+        print(defect)
+        return defect
+    elif response["type"] == RecordItemResponseType.SCANNED_ITEM:
+        item_connector.scan_item(item_id=response["item_id"])
+# @router.websocket("/dashboard")
+# async def socket_test(websocket: WebSocket):
+#     await dashboard_manager.connect(websocket, user_id)
+#     try:
+#         while True:
+#             response = await websocket.receive_json()
+#     except WebSocketDisconnect:
+#         print("Connection closed")
