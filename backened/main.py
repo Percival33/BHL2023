2b6135019a78c6792522343afe717dd8efdd3542
mszawerd <01169246@pw.edu.pl>
2023-04-15 14:09:40 +0200

    basic setup

--- backened/main.py
+++ backened/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from pymongo.mongo_client import MongoClient
 
-client = MongoClient("mongodb+srv://bhl:<password>@cluster0.eyuhfo8.mongodb.net/?retryWrites=true&w=majority")
+from models import UserModel
+
+client = MongoClient()
 app = FastAPI()
+db = client['dev']
 
 
-@app.get("/")
+@app.get("/", response_model=list[UserModel])
 def root():
-    return {"message": "Hello World"}
+    return list(db['user'].find({}))
 
 
 @app.get("/hello/{name}")
 def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return ""
