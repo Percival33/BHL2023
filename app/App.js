70863c4dc27442f2238444d19b86f103c4266b53
dawidkasz <dkaszynski04@gmail.com>
2023-04-15 17:11:16 +0200

    Basic structure

--- app/App.js
+++ app/App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from "expo-font";
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Main from "./src/Main";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Nunito-Regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-SemiBold': require('./assets/fonts/Nunito-Bold.ttf'),
+    'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  });
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaProvider>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Main />
+        </NavigationContainer>
+      </SafeAreaProvider>
   );
 }
 
