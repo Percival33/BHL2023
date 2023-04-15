0eb66fd0473cca4e1a43bc088ec00f56c98ef18c
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-04-15 14:37:21 +0200

    add Settings class from dotenv file

--- backened/main.py
+++ backened/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from pymongo.mongo_client import MongoClient
 
-from models import UserModel
+from .models import UserModel
+from .config import settings
+import uvicorn
 
-client = MongoClient()
+client = MongoClient(f"{settings.DB_URL}/{settings.DB_NAME}")
 app = FastAPI()
 db = client['dev']
 
@@ -16,3 +18,12 @@ def root():
 @app.get("/hello/{name}")
 def say_hello(name: str):
     return ""
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.HOST,
+        reload=settings.DEBUG_MODE,
+        port=settings.PORT,
+    )
