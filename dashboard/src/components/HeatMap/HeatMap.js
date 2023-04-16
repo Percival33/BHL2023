0f815ec58f2180e15a928dbc29f889b229dc552c
Kuba <01169177@pw.edu.pl>
2023-04-16 10:52:50 +0200

    heatmap works

--- dashboard/src/components/HeatMap/HeatMap.js
+++ dashboard/src/components/HeatMap/HeatMap.js
@@ -1,48 +1,56 @@
 import React, { useState, useEffect } from "react";
 
 const HeatMap = () => {
-    // dummy data
-    const data = [
-        [0, 0],
-        [0, 0],
-        [0, 0],
-        [0, 0],
-    ];
+  // dummy data
+  const data = [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+  ];
 
-    const [map, setMap] = useState([])
+  const [map, setMap] = useState([]);
 
-    useEffect(() => {
-        const fetchMap = async () => {
-            const data = await (await fetch(`http://localhost:8000/dashboard/heatmap`)).json()
-            console.log(data)
-            setMap(data)
-        }
-        fetchMap()
-    }, [])
+  useEffect(() => {
+    const fetchMap = async () => {
+      const data = await (
+        await fetch(`http://localhost:8000/dashboard/heatmap`)
+      ).json();
+      console.log(data);
+      setMap(data);
+    };
+    fetchMap();
+  }, []);
 
-    map.forEach((point)=>{
-        data[point.regal-1][point.column-1] +=point.qty
-    })
+  map.forEach((point) => {
+    data[point.regal - 1][point.column - 1] += point.qty;
+  });
 
+  // find the maximum value in the data
+  const max = Math.max(...data.map((row) => Math.max(...row)));
 
-    // generate the heatmap cells
-    const cells = data.map((row, rowIndex) =>
-        row.map((value, colIndex) => (
-            <div
-                key={`${rowIndex}-${colIndex}`}
-                className={`py-3 px-6 text-center ${value > 10 ? "bg-red-500" : "bg-green-500"
-                    }`}
-            >
-                {value}
-            </div>
-        ))
-    );
-
-    return (
-        <div className="h-full w-full flex justify-center items-center">
-            <div className="grid grid-cols-2 grid-rows-4 gap-2">{cells}</div>
+  // generate the heatmap cells
+  const cells = data.map((row, rowIndex) =>
+    row.map((value, colIndex) => {
+      // calculate the color based on the value
+      const color = `hsl(${120 - (value / max) * 120}, 100%, 50%)`;
+      return (
+        <div
+          key={`${rowIndex}-${colIndex}`}
+          className="py-3 px-6 text-center"
+          style={{ backgroundColor: color }}
+        >
+          {value}
         </div>
-    );
+      );
+    })
+  );
+
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <div className="grid grid-cols-2 grid-rows-4 gap-2">{cells}</div>
+    </div>
+  );
 };
 
 export default HeatMap;
