f43c82b6e701eb9db774d46fa21b189d7a646f6a
Kuba <01169177@pw.edu.pl>
2023-04-16 04:44:48 +0200

    removed backend, app

--- backened/connectors/ABCConnector.py
+++
@@ -1,9 +0,0 @@
-from bson import ObjectId
-from pymongo import MongoClient
-from abc import ABC
-
-
-class DatabaseConnector(ABC):
-    def __init__(self, url, db_name='dev'):
-        self.client = MongoClient(url)
-        self.db = self.client[db_name]
