9b8bae4788386267527990e6d7ce8ec028de1f2a
mszawerd <01169246@pw.edu.pl>
2023-04-16 04:34:19 +0200

    added order-queue

--- backened/managers/RecordManager.py
+++ backened/managers/RecordManager.py
@@ -5,18 +5,21 @@ from managers.PathManager import PathManager
 from models import Order, Record, RecordItem
 import datetime
 
+from utils.RecordItemsQueue import RecordItemsQueue
+
 
 class RecordManager:
     def __init__(self, user_connector: UserConnector, product_connector: ProductConnector, path_manager: PathManager):
         self.user_connector = user_connector
         self.product_connector = product_connector
         self.path_manager = path_manager
+        self.records_queue = RecordItemsQueue()
 
     def reserve_products(self, order_items):
         taken = []
         for item in order_items:
-            products = self.product_connector.get_available_products_by_type(item['product_type_id'])
-            to_take = item['qty']
+            products = self.product_connector.get_available_products_by_type(item.product_type_id)
+            to_take = item.qty
             for product in products:
                 count = product['count']
                 possible_to_take = count - product['count_reserved']
@@ -31,23 +34,30 @@ class RecordManager:
 
     #TODO make it in transaction
     def handle_order(self, order: Order):
-        taken = self.reserve_products(order['order_items'])
-        user = self.user_connector.get_free_users()[0]
-        self.user_connector.change_user_state(user, True)
-        record = Record(**{
-            "worker_id": user['user_id'],
-            "date_started": datetime.datetime.now().isoformat(),
-            "products": [
-                RecordItem(**{
-                    'product_id': str(product['_id']),
-                    'qty': qty,
-                    'regal': product['regal'],
-                    'column': product['column'],
-                    'shelf': product['shelf']
-                }) for product, qty in taken
-            ],
-            "distance": -1
-        })
-        record.products = self.path_manager.get_optimal_route(record.products)
-        return record
+        self.records_queue.inject_order(order)
+        def is_free_user():
+            return self.user_connector.get_free_user_count() > 0
+        records = []
+        while self.records_queue.is_enough_to_assign() and is_free_user():
+            batch = self.records_queue.get_batch()
+            taken = self.reserve_products(batch)
+            user = self.user_connector.get_free_users()[0]
+            self.user_connector.change_user_state(user, True)
+            record = Record(**{
+                "worker_id": user['user_id'],
+                "date_started": datetime.datetime.now().isoformat(),
+                "products": [
+                    RecordItem(**{
+                        'product_id': str(product['_id']),
+                        'qty': qty,
+                        'regal': product['regal'],
+                        'column': product['column'],
+                        'shelf': product['shelf']
+                    }) for product, qty in taken
+                ],
+                "distance": -1
+            })
+            record.products = self.path_manager.get_optimal_route(record.products)
+            records.append(record)
+        return records
 
