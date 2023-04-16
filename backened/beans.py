7aeb32132e49c0a909395cba303ef51da88fd0a0
mszawerd <01169246@pw.edu.pl>
2023-04-16 03:22:25 +0200

    bf optimal route

--- backened/beans.py
+++ backened/beans.py
@@ -4,6 +4,7 @@ from connectors.UserConnector import UserConnector
 from connectors.DefectConnector import DefectConnector
 
 from config import settings
+from managers.PathManager import PathManager
 
 from managers.RecordManager import RecordManager
 from managers.UserManagers import UserManager
@@ -13,5 +14,6 @@ item_connector = ItemConnector(settings.DB_URL)
 product_connector = ProductConnector(settings.DB_URL)
 defect_connector = DefectConnector(settings.DB_URL)
 
-records_manager = RecordManager(user_connector, product_connector)
 user_manager = UserManager()
+path_manager = PathManager()
+records_manager = RecordManager(user_connector, product_connector, path_manager)
--- backened/test_tsp.py
+++ backened/test_tsp.py
@@ -1,6 +1,9 @@
 import numpy as np
 from python_tsp.exact import solve_tsp_dynamic_programming
-
+from dijkstar import Graph, find_path
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import floyd_warshall
+from itertools import permutations
 graph = {
     0: [(2, 2), (3, 2), (6, 5), (7, 5)],
     1: [(2, 1)],
@@ -13,11 +16,30 @@ graph = {
     8: []
 }
 
-
 distance_matrix = np.zeros((9, 9))
 for key, item in graph.items():
     for n, d in item:
-        distance_matrix[key][d]
+        distance_matrix[key, n] = d
+        distance_matrix[n, key] = d
 
-permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
-print(permutation, distance)
+path = [8, 2, 5]
+
+dist_matrix, pre = floyd_warshall(csgraph=csr_matrix(distance_matrix), directed=False, return_predecessors=True)
+best_route = []
+dist = float("inf")
+for p in permutations(path):
+    p = list([0, *p, 0])
+    prev = p[0]
+    d = 0
+    for n in p[1:]:
+        d += dist_matrix[prev, n]
+        prev = n
+    if d < dist:
+        best_route = p
+        dist = d
+
+
+a = [((1, 2), [1]), ((2, 3), [3])]
+c, d = list(zip(*a))
+print(c)
+print(d)
